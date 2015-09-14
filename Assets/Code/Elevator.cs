b9299a0fb39c2586dee417070cf05bf8cb593ad3
taichiu727 <taichiuchiu727@gmail.com>
2015-09-14 13:16:58 +0800

    update

---
+++ Assets/Code/Elevator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	 
+	public GameObject directionObj; 
+	public float x,y,z; 
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {				
+		      
+	}
+
+	void moveUp() 
+	{ 
+		transform.position += new Vector3(0,4,0); 
+	}
+
+
+
+	void OnTriggerEnter(Collider other) 
+	{ 
+		if(Input.GetKeyDown(KeyCode.Alpha1))
+		{ 
+			print("one"); 
+			moveUp (); 
+		}
+		print("one"); 
+		moveUp (); 
+
+	}
+}
---
+++ Assets/Code/ShipMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipMovement : MonoBehaviour {
+	public bool move = false; 
+	public GameObject directionObj; 
+	public float x,y,z; 
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(move) 
+		{ 				
+			transform.position += new Vector3(x,y,z);       
+		}
+			
+	}
+	void Awake() {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+	
+}
