b2755c93baab42f04c3832764138daf1728d3eac
taichiu727 <taichiuchiu727@gmail.com>
2015-09-16 13:49:06 +0800

    alex

--- Assets/Code/Elevator.cs
+++ Assets/Code/Elevator.cs
@@ -5,7 +5,7 @@ public class Elevator : MonoBehaviour {
 
 	 
 	public Transform F1,F2,F3; 
-	
+	public bool floor1, floor2, floor3 = false; 
 
 	// Use this for initialization
 	void Start () {
@@ -15,24 +15,44 @@ public class Elevator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//elevator moves up 
-		
+		if(floor1)
+		{ 
+			transform.position = Vector3.MoveTowards(transform.position, F1.position, Time.deltaTime*10F);
+			
+		}
+		if(floor2)
+		{ 
+			transform.position = Vector3.MoveTowards(transform.position, F2.position, Time.deltaTime*10F);
+			
+		}
+		if(floor3)
+		{ 
+			transform.position = Vector3.MoveTowards(transform.position, F3.position, Time.deltaTime*10F);
+			
+		}
 
 	}
 	
 	void OnTriggerStay(Collider other) 
 	{ 
 		if (Input.GetKey (KeyCode.Alpha1)) {
-			transform.position = Vector3.MoveTowards(transform.position, F1.position, Time.deltaTime*10F);
+			floor1 = true;
+			floor2 = false; 
+			floor3 = false; 
 			//transform.position += transform.up * Time.deltaTime * 10.0f;
 		}
 		
 		if (Input.GetKey (KeyCode.Alpha2)) {
-			transform.position = Vector3.MoveTowards(transform.position, F2.position, Time.deltaTime);
+			floor2 = true; 
+			floor1 = false; 
+			floor3 = false; 
 			//transform.position -= transform.up * Time.deltaTime * 5.0f;
 		}
 		//elevator moves down 
 		if (Input.GetKey (KeyCode.Alpha3)) {
-			transform.position = Vector3.MoveTowards(transform.position, F3.position, Time.deltaTime);
+			floor3 = true; 
+			floor1 = false; 
+			floor2 = false; 
 			//transform.position -= transform.up * Time.deltaTime * 5.0f;
 		}
 	}
